92664b720b83eefe5afc0e9493e79374f6305042
Laugiwa <17611038@students.uii.ac.id>
2019-12-12 21:44:12 +0700

    menambahkan komentar

---
+++ cobagabung.py
@@ -0,0 +1,35 @@
+#tambah komenta disini
+
+import cv2, time
+from PIL import Image
+camera = 0
+video = cv2.VideoCapture(camera,cv2.CAP_DSHOW)
+a = 0
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+recognizer.read('C:/face/tangkap/training/training.xml')
+id = 0
+fontFace = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+fontColor = (255,0,0)
+while True:
+  check, frame = video.read()  
+  print(check)
+  print(frame)  
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  faces=faceDetect.detectMultiScale(gray,1.3,5);
+  for (x,y,w,h) in faces:
+      a = a+1
+      cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+      id, conf=recognizer.predict(gray[y:y+h,x:x+w])
+      if (id==1):
+          id = "Lau" 
+      elif (id==2):
+          id = "Ita"   
+      cv2.putText(frame,str(id),(x+w,y+h),fontFace,fontScale,fontColor)
+  cv2.imshow("wajah",frame)
+  if (cv2.waitKey(1)==ord('q')):
+       break
+  print(a)
+video.release()
+cv2.destroyAllWindows()
